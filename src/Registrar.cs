a6e50cc8b2acd2a0666c1c85218aca7bc8226432
Harry <harrypierson@outlook.com>
2020-12-09 13:23:37 -0800

    initial commit

---
+++ src/Registrar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+
+namespace DevHawk.Contracts
+{
+    public class Registrar : SmartContract
+    {
+    }
+}
---
+++ test/RegistrarTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test.DevHawk.Contracts
+{
+    public class RegistrarTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
