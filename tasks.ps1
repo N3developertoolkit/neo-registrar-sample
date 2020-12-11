bb9276cde038ac9e0e9e00fe2ffd795c2d46b756
Harry <harrypierson@outlook.com>
2020-12-11 10:16:21 -0800

    update to use shipping neon/nxp3

--- tasks.ps1
+++ tasks.ps1
@@ -1,12 +1,12 @@
 dotnet tool restore
-dotnet build C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj
+mkdir ./checkpoints -force | out-null
 
 dotnet build ./src
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- reset -f
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- transfer gas 10000 genesis owen
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- transfer gas 10000 genesis alice
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- transfer gas 10000 genesis bob
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- contract deploy ./src/bin/Debug/netstandard2.1/registrar.nef owen
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- checkpoint create checkpoints/contract-deployed -f
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- contract invoke ./invoke-files/register-sample-domain.neo-invoke.json bob
-dotnet run -p C:\Users\harry\Source\neo\seattle\express\src\nxp3\nxp3.csproj --no-build -- checkpoint create checkpoints/sample-domain-registered -f
+dotnet nxp3 reset -f
+dotnet nxp3 transfer gas 10000 genesis owen
+dotnet nxp3 transfer gas 10000 genesis alice
+dotnet nxp3 transfer gas 10000 genesis bob
+dotnet nxp3 contract deploy ./src/bin/Debug/netstandard2.1/registrar.nef owen
+dotnet nxp3 checkpoint create checkpoints/contract-deployed -f
+dotnet nxp3 contract invoke ./invoke-files/register-sample-domain.neo-invoke.json bob
+dotnet nxp3 checkpoint create checkpoints/sample-domain-registered -f
